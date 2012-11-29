5d8754e9c1d4f742eed4631c69b7697be09a604f
kufikugel <kufikugel@googlemail.com>
2012-11-29 04:16:15 +0100

    toro touches 4.2.1
    PatchSet 2
    
    add widivine and nxp in dependencies
    
    PatchSet 3
    
    change release name to codename toro

---
+++ slim.mk
@@ -0,0 +1,41 @@
+# Boot animation
+TARGET_BOOTANIMATION_NAME := vertical-720x1280
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/toro/full_toro.mk)
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+# Inherit device settings
+$(call inherit-product, vendor/slim/config/common_nexus.mk)
+
+# Inherit torch settings
+$(call inherit-product, vendor/slim/config/common_ledflash.mk)
+
+PRODUCT_COPY_FILES +=  \
+    vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.camera.res=5MP
+
+#copy 00check
+PRODUCT_COPY_FILES += \
+    vendor/slim/prebuilt/common/etc/init.d/00check:system/etc/init.d/00check
+
+#copy vold.fstab
+PRODUCT_COPY_FILES += \
+    device/samsung/toro/vold.fstab:system/etc/vold.fstab
+
+# Release name
+PRODUCT_RELEASE_NAME := toro
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=mysid BUILD_FINGERPRINT="google/mysid/toro:4.2.1 JOP40D eng.ar.20121128.230418 release-keys"
+PRIVATE_BUILD_DESC="mysid-userdebug 4.2.1 JOP40D 230418 release-keys"
+
+PRODUCT_NAME := slim_toro
+PRODUCT_DEVICE := toro
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Galaxy Nexus
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_RESTRICT_VENDOR_FILES := false
